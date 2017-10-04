ac1a050f20b619481fce658892b60c8379389a5a
RakiRoad <quintor2@tcnj.edu>
2017-10-04 13:04:22 -0400

    Updated Lab
    Lab 1

--- Lab 1/Lab1.m
+++ Lab 1/Lab1.m
@@ -1,6 +1,6 @@
 fs = 16000
 x = audioread('C:\Users\Rocky\Documents\GitHub\Speech Processing\Lab 1\SA1.wav', 'double');
-%audioinfo('C:\TIMIT\TRAIN\DR1\FCJF0\SA1.wav');
+audioinfo('C:\Users\Rocky\Documents\GitHub\Speech Processing\Lab 1\SA1.wav')
 
 %==============================================================
 %finds the total number of samples
@@ -21,15 +21,97 @@ for i = 1:147
 %     plot(y(:,i))%plots each column
 %     figure %makes it so that each plot creates a new figure
 end
-hold off; 
+hold off;
 
 %calculates the total power of each window and stores it in the array
 %'total_sample_powers'
-total_sample_powers = zeros(147,1)
+total_sample_powers = zeros(147,1);
+total_fft = zeros(320,147);
 hold on 
 for i = 1:147
     F = fft(y(:,i));
+    total_fft(:,i) = fft(y(:,i));
     pow = F.*conj(F);
-    total_sample_powers(i,1) = sum(pow)
+    total_sample_powers(i,1) = sum(pow);
 end
 hold off
+
+
+tempval = zeros(147,1);
+for i = 1:147
+    tempval(i:1) = i;
+end
+
+plot(y(:,25))
+title('ham 25')
+figure
+plot(x)
+figure
+
+plot(total_sample_powers,'-o')
+title('power all')
+
+
+
+%bar(total_fft(:,1))
+
+
+meme = fft(y(:,25));
+
+
+%========
+% sze = 320;
+% abs_area = abs(meme);
+% y_fft = abs_area(1:160);
+% FF = fs*(0:160-1)/320;
+% figure
+% plot(FF, y_fft, '-x')
+% title('fft 1')
+% xlabel('freq and hz');
+% ylabel('Amplitude');
+% figure
+%=====
+
+Thresh = zeros(147:1)
+for j = 1:147
+    count = 0;
+    for i = 1:320
+        if i > 1
+            if y(i-1,j)*y(i,j)<0 
+                count = count + 1;
+            end
+        end
+    end
+    Thresh (j,1) = count;
+end
+
+plot(Thresh, '-o')
+title('Thresh')
+
+%Code to figure out vowel and stuff
+voice1 = zeros(160:147);
+for i = 1:147
+    ham1 = fft(y(:,i));
+    sze = 320;
+    abs_area = abs(ham1);
+    y_fft = abs_area(1:160);
+    FF = fs*(0:160-1)/320;
+    
+    [val, index] = max(y_fft);
+    idx_max = FF(1:index);
+    
+    for j = 1:160
+        if idx_max > 840
+            voice1(j:i) = 1
+        elseif idx_max >= 840 & idx_max <1380
+            voice (j:i) = 2
+        elseif idx_max >= 1380 & idx_max <1920
+            voice (j:i) = 3
+        elseif idx_max >= 1920 & idx_max <2460
+            voice (j:i) = 4
+        elseif idx_max >= 2460
+            voice (j:i) = 5
+        end
+    end
+end
+            
